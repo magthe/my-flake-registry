d86489300e145050a8c338072b3acc849cc2388d
Magnus Therning <magnus@therning.org>
2025-05-03 08:44:15 +0200

    Add a flake with a devshell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  # nixpkgs and flake-ustils come from the registry
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with nixpkgs.legacyPackages.${system};
+      let
+        hsPkgs = haskell.packages.ghc96;
+        hsPkgsFn = p: [ p.hello ];
+      in {
+        devShells.default = mkShell {
+          buildInputs = with hsPkgs; [
+            (ghcWithPackages hsPkgsFn)
+            hlint
+            # haskell-language-server
+          ];
+        };
+      });
+
+  nixConfig = {
+    flake-registry = 
+      "https://raw.githubusercontent.com/magthe/my-flake-registry/main/flake-registry.json";
+    extra-substituters = [ "https://magthe-dev.cachix.org" ];
+    extra-trusted-public-keys = [
+      "magthe-dev.cachix.org-1:mFSHoML7X60LmeIf+vvmuG6uBHdufg8kcUkcfSV3yHc="
+    ];
+  };
+}
